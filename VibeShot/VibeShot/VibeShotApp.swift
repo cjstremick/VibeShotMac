1aebbf8d48d2ef7b9156480a795d483e58a2d0b2
Cj Stremick <cj.stremick@watco.com>
2025-09-14 11:19:42 -0500

    chore: Project stub.

---
+++ VibeShot/VibeShot/VibeShotApp.swift
@@ -0,0 +1,15 @@
+//
+//  VibeShotApp.swift
+//  VibeShot
+//
+//  Created by Cj Stremick on 9/14/25.
+//
+
+import SwiftUI
+
+@main
+struct VibeShotApp: App {
+    var body: some Scene {
+        Settings { EmptyView() }
+    }
+}
